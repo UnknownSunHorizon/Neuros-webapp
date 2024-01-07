bfe43962327243cb574ba33ebff73ed65155e260
UnknownSunHorizon <peshkosergey37@gmail.com>
2024-01-07 21:20:01 +0600

    Изменил шапку

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom"
 import MenuItem from "antd/es/menu/MenuItem"
 import { Menu } from "antd"
-import header_logo from "./assets/header.png"
+import header_logo from "./assets/logo.svg"
 
 const Header = () => {
     return (
         <header>                      
             <Menu theme="dark" mode="horizontal" >
                 <img src={header_logo} height="40" />
-                <MenuItem><Link to="/">Главная</Link></MenuItem> 
-                <MenuItem><Link to="/about">О сайте</Link></MenuItem>
-                <MenuItem><Link to="/reviews">Отзывы пользователей</Link></MenuItem>
+                <MenuItem><Link to="/">Нейросети</Link></MenuItem> 
+                <MenuItem><Link to="/pipelist">Пайплайны</Link></MenuItem>
+                <MenuItem><Link to="/reviews">Профиль</Link></MenuItem>
+                <MenuItem><Link to="/reviews">Настройки</Link></MenuItem>
+                
             </Menu>
         </header>
     )

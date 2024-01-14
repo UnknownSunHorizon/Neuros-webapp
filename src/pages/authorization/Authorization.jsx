8a441e1d1245e4eb9e2974451788301ae18a9d5f
Parygin-Vyacheslav <vyascheslav.boxgorn@gmail.com>
2024-01-14 19:47:21 +0600

    Доделаны регистрация и авторизация

--- src/pages/authorization/Authorization.jsx
+++ src/pages/authorization/Authorization.jsx
@@ -2,26 +2,54 @@ import styles from './styles/authorization.module.css';
 import React from 'react';
 import {Link} from "react-router-dom";
 
+import gmail_logo from "./assets/gmail_logo.jpg";
+import yamail_logo from "./assets/ymail_logo.jpg";
+import mail_logo from "./assets/mail_logo.jpg";
+
 export const Authorization = () => {
     return (
         <div id={styles.authorization_background}>
             <div id={styles.authorization_container}>
                 <div id={styles.authorization_title}>Авторизация</div>
-                <div id={styles.authorization_inputblock}>
-                    <div className={styles.authorization_login}>
-                        <span className={styles.authorization_subtitle}>Почта</span>
-                        <input className={styles.authorization_input} type="text"></input>
+                <form id={styles.authorization_form} action="/profile">
+                    <div id={styles.authorization_inputblock}>
+                        <div className={styles.authorization_login}>
+                            <span className={styles.authorization_subtitle}>Почта</span>
+                            <input className={styles.authorization_input} type="email" required/>
+                        </div>
+                        <div className={styles.authorization_login}>
+                            <span className={styles.authorization_subtitle}>Пароль</span>
+                            <input className={styles.authorization_input} 
+                            type="password" 
+                            minLength="8" maxLength="24" size="24" 
+                            pattern="[a-Z0-9]" required
+                            title="Требования к паролю:
+                             - содержит латинские буквы a-Z;
+                             - содержит цифры 0-9;
+                             - длина пароля 8-24 символов."/>
+                            <p id={styles.authorization_memory}>Забыли пароль?</p>
+                        </div>
+                        
                     </div>
-                    <div className={styles.authorization_login}>
-                        <span className={styles.authorization_subtitle}>Пароль</span>
-                        <input className={styles.authorization_input} type="text"></input>
-                    </div>
-                    <div className={styles.authorization_login}>
-                        <span className={styles.authorization_subtitle}>Повторите пароль</span>
-                        <input className={styles.authorization_input} type="text"></input>
-                    </div>
+                    <input id={styles.authorization_button} type="submit" value="Войти" />
+                </form>
+                <span id={styles.authorization_toRegistration}>Нет аккаунта? <span />
+                    <Link to="/register">
+                        <a id={styles.authorization_link}>Зарегистрироваться</a>
+                    </Link>
+                </span>
+                <span id={styles.authorization_with_help}>Или войдите с помощью</span>
+                <div id={styles.authorization_icons}>
+                    <a  href="https://mail.google.com/" target="_blank">
+                        <img id={styles.authorization_icon_1} alt="gmail_logo" src={gmail_logo} />
+                    </a>
+                    <a href="https://account.mail.ru/" target="_blank">
+                        <img id={styles.authorization_icon_2} alt="mail_logo" src={mail_logo} />
+                    </a>
+                    <a href="https://mail.yandex.ru/" target="_blank">
+                        <img id={styles.authorization_icon_3} alt="yamail_logo" src={yamail_logo} />
+                    </a>
                 </div>
-                
             </div>                        
         </div>
     );
--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -2,32 +2,46 @@ import styles from './styles/registration.module.css';
 import React from 'react';
 import {Link} from "react-router-dom";
 
-import vk_logo from "./assets/vk_logo.png";
-import google_logo from "./assets/google_logo.jpg";
-import facebook_logo from "./assets/facebook_logo.svg";
+import gmail_logo from "./assets/gmail_logo.jpg";
+import ymail_logo from "./assets/ymail_logo.jpg";
+import mail_logo from "./assets/mail_logo.jpg";
 
 export const Registration = () => {
     return (
         <div id={styles.registration_background}>
             <div id={styles.registration_container}>
                 <div id={styles.registration_title}>Регистрация</div>
-                <div id={styles.registration_inputblock}>
-                    <div className={styles.registration_login}>
-                        <span className={styles.registration_subtitle}>Почта</span>
-                        <input className={styles.registration_input} type="text"></input>
+                <form id={styles.registration_form} action="/profile">
+                    <div id={styles.registration_inputblock}>
+                        <div className={styles.registration_login}>
+                            <span className={styles.registration_subtitle}>Почта</span>
+                            <input className={styles.registration_input} type="email" required></input>
+                        </div>
+                        <div className={styles.registration_login}>
+                            <span className={styles.registration_subtitle}>Пароль</span>
+                            <input className={styles.registration_input} 
+                            type="password" 
+                            minLength="8" maxLength="24" size="24" 
+                            pattern="[a-zA-Z0-9]" required
+                            title="Требования к паролю:
+                             - содержит латинские буквы a-Z;
+                             - содержит цифры 0-9;
+                             - длина пароля 8-24 символов." />
+                        </div>
+                        <div className={styles.registration_login}>
+                            <span className={styles.registration_subtitle}>Повторите пароль</span>
+                            <input className={styles.registration_input}  
+                            type="password" 
+                            minLength="8" maxLength="24" size="24" 
+                            pattern="[a-zA-Z0-9]" required
+                            title="Требования к паролю:
+                             - содержит латинские буквы a-Z;
+                             - содержит цифры 0-9;
+                             - длина пароля 8-24 символов." />
+                        </div>
                     </div>
-                    <div className={styles.registration_login}>
-                        <span className={styles.registration_subtitle}>Пароль</span>
-                        <input className={styles.registration_input} type="text"></input>
-                    </div>
-                    <div className={styles.registration_login}>
-                        <span className={styles.registration_subtitle}>Повторите пароль</span>
-                        <input className={styles.registration_input} type="text"></input>
-                    </div>
-                </div>
-                <Link to="/profile">
-                    <button id={styles.registration_button}>Зарегистрироваться</button>
-                </Link>
+                    <input id={styles.registration_button} type="submit" value="Зарегистрироваться" />
+                </form>
                 <span id={styles.registration_toAuthorization}>Уже есть аккаунт? <span />
                     <Link to="/authorize">
                         <a id={styles.registration_link}>Войти</a>
@@ -35,9 +49,15 @@ export const Registration = () => {
                 </span>
                 <span id={styles.registration_with_help}>Или зарегистрируйтесь с помощью</span>
                 <div id={styles.registration_icons}>
-                    <div id={styles.registration_icon_1}><img id={styles.registration_vk} alt="vk_logo" src={vk_logo} /></div>
-                    <img id={styles.registration_icon_2} alt="google_logo" src={google_logo} />
-                    <img id={styles.registration_icon_3} alt="facebook_logo" src={facebook_logo} />
+                    <a  href="https://mail.google.com/" target="_blank">
+                        <img id={styles.registration_icon_1} alt="gmail_logo" src={gmail_logo} />
+                    </a>
+                    <a href="https://account.mail.ru/" target="_blank">
+                        <img id={styles.registration_icon_2} alt="mail_logo" src={mail_logo} />
+                    </a>
+                    <a href="https://mail.yandex.ru/" target="_blank">
+                        <img id={styles.registration_icon_3} alt="ymail_logo" src={ymail_logo} />
+                    </a>
                 </div>
             </div>                        
         </div>
